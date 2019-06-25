9cc26e13779e40de35afa757995569f3ca00dc25
Chung Bui <chungbd90@gmail.com>
2019-06-25 08:35:07 +0700

    add drowpdown button and expansable card

---
+++ dropdown_button/lib/main.dart
@@ -0,0 +1,58 @@
+// Flutter code sample for material.DropdownButton.1
+
+// This sample shows a `DropdownButton` whose value is one of
+// "One", "Two", "Free", or "Four".
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+/// This Widget is the main application widget.
+class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: Text(_title)),
+        body: MyStatefulWidget(),
+      ),
+    );
+  }
+}
+
+String dropdownValue = 'One';
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: 
+          DropdownButton<String>(
+            value: dropdownValue,
+            onChanged: (String newValue) {
+              setState(() {
+                dropdownValue = newValue;
+              });
+            },
+            items: <String>['One', 'Two', 'Free', 'Four']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
+          ),
+      ),
+    );
+  }
+}
---
+++ expansable_card/lib/main.dart
@@ -0,0 +1,105 @@
+// Flutter code sample for material.ExpansionPanelList.1
+
+// Here is a simple example of how to implement ExpansionPanelList.
+
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+/// This Widget is the main application widget.
+class MyApp extends StatelessWidget {
+  static const String _title = 'Flutter Code Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: _title,
+      home: Scaffold(
+        appBar: AppBar(title: Text(_title)),
+        body: MyStatefulWidget(),
+      ),
+    );
+  }
+}
+
+// stores ExpansionPanel state information
+class Item {
+  Item({
+    this.expandedValue,
+    this.headerValue,
+    this.isExpanded = false,
+  });
+
+  String expandedValue;
+  String headerValue;
+  bool isExpanded;
+}
+
+List<Item> generateItems(int numberOfItems) {
+  return List.generate(numberOfItems, (int index) {
+    return Item(
+      headerValue: 'Panel $index',
+      expandedValue: 'This is item number $index',
+    );
+  });
+}
+
+class MyStatefulWidget extends StatefulWidget {
+  MyStatefulWidget({Key key}) : super(key: key);
+
+  @override
+  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
+}
+
+class _MyStatefulWidgetState extends State<MyStatefulWidget> {
+  List<Item> _data = generateItems(8);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Container(
+        child: _buildPanel(),
+      ),
+    );
+  }
+
+  Widget _buildPanel() {
+    return ExpansionPanelList(
+      expansionCallback: (int index, bool isExpanded) {
+        print("index $index $isExpanded");
+        setState(() {
+          _data[index].isExpanded = !isExpanded;
+        });
+      },
+      children: _data.map<ExpansionPanel>((Item item) {
+        return ExpansionPanel(
+          headerBuilder: (BuildContext context, bool isExpanded) {
+            return Column(children: <Widget>[
+              ListTile(
+                title: Text(item.headerValue),
+                trailing: Icon(Icons.ac_unit),
+              ),
+
+              ListTile(
+                title: Text(item.headerValue),
+                trailing: Icon(Icons.ac_unit),
+              ),
+
+            ],);
+          },
+          body: ListTile(
+              title: Text(item.expandedValue),
+              subtitle: Text('To delete this panel, tap the trash can icon'),
+              trailing: Icon(Icons.delete),
+              onTap: () {
+                setState(() {
+                  _data.removeWhere((currentItem) => item == currentItem);
+                });
+              }),
+          isExpanded: item.isExpanded,
+
+        );
+      }).toList(),
+    );
+  }
+}
