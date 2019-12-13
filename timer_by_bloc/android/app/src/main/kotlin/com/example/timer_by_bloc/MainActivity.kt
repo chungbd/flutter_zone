953b8faf4f770fb326faf590da9be0bbf717db67
Chung Bui <chungbd90@gmail.com>
2019-12-13 16:30:42 +0700

    add timer by bloc project

---
+++ timer_by_bloc/android/app/src/main/kotlin/com/example/timer_by_bloc/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.timer_by_bloc
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
