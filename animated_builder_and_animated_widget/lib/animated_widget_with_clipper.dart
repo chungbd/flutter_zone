a52d8b8b1f55d53300145c05668716da70a4ac8b
Chung Bui <chungbd90@gmail.com>
2019-12-16 18:37:38 +0700

    update resource for image

---
+++ animated_builder_and_animated_widget/lib/animated_widget_with_clipper.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage>
+    with SingleTickerProviderStateMixin {
+  final Image starsBackground = Image.asset(
+    'assets/milky-way.jpg',
+  );
+  final Image ufo = Image.asset('assets/ufo.jpg');
+  AnimationController _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _animation = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this,
+    )..repeat();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      alignment: AlignmentDirectional.center,
+      children: <Widget>[
+        starsBackground,
+        BeamTransition(animation: _animation),
+        ufo,
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    _animation.dispose();
+    super.dispose();
+  }
+}
+
+class BeamTransition extends AnimatedWidget {
+  BeamTransition({Key key, Animation<double> animation})
+      : super(key: key, listenable: animation);
+  @override
+  Widget build(BuildContext context) {
+    final Animation<double> animation = listenable;
+    return ClipPath(
+      clipper: const BeamClipper(),
+      child: Container(
+        height: 1000,
+        decoration: BoxDecoration(
+          gradient: RadialGradient(
+            radius: 1.5,
+            colors: [
+              Colors.yellow,
+              Colors.transparent,
+            ],
+            stops: [0, animation.value],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class BeamClipper extends CustomClipper<Path> {
+  const BeamClipper();
+
+  @override
+  getClip(Size size) {
+    return Path()
+      ..lineTo(size.width / 2, size.height / 2)
+      ..lineTo(size.width, size.height)
+      ..lineTo(0, size.height)
+      ..lineTo(size.width / 2, size.height / 2)
+      ..close();
+  }
+
+  /// Return false always because we always clip the same area.
+  @override
+  bool shouldReclip(CustomClipper oldClipper) => false;
+}
