87094d7af8fae8ff05f82817510b6305f90fca59
Chung Bui <chungbd90@gmail.com>
2021-09-17 23:51:42 +0700

    day 2: animated position

--- animation_daily/lib/constrains/screens.dart
+++ animation_daily/lib/constrains/screens.dart
@@ -1,6 +1,7 @@
-import 'package:animation_daily/screens/animated_positioned_screen.dart';
-import 'package:animation_daily/screens/animated_size_screen.dart';
 import 'package:flutter/widgets.dart';
+
+import '../screens/animated_positioned_screen.dart';
+import '../screens/animated_size_screen.dart';
 
 class ScreenInfor {
   final String name;
@@ -15,15 +16,19 @@ class ScreenInfor {
 
 List<ScreenInfor> screens = [
   ScreenInfor(
-      name: "Day 1: Animated Size",
-      description: "The animation only will appears the first time!",
-      instance: (infor) => AnimatedSizeScreen(
+      name: "Day 2: Animated Positioned",
+      description:
+          """With AnimatedPositioned, we can change the property of size: 
+          the padding of top, bottom, left and right; 
+          Size of Container: width, hight.
+          """,
+      instance: (infor) => AnimatedPositionedScreen(
             infor: infor,
           )),
   ScreenInfor(
-      name: "Day 2: Animated Positioned",
-      description: "",
-      instance: (infor) => AnimatedPositionedScreen(
+      name: "Day 1: Animated Size",
+      description: "The animation only will appears the first time!",
+      instance: (infor) => AnimatedSizeScreen(
             infor: infor,
           )),
 ];
--- animation_daily/lib/screens/animated_positioned_screen.dart
+++ animation_daily/lib/screens/animated_positioned_screen.dart
@@ -13,13 +13,82 @@ class AnimatedPositionedScreen extends StatefulWidget {
       _AnimatedPositionedScreenState();
 }
 
+enum Positions { bottom, top, all }
+
 class _AnimatedPositionedScreenState extends State<AnimatedPositionedScreen>
     with ScaffoldBase {
+  Positions _position = Positions.bottom;
+
   @override
   Widget createBody(BuildContext context) {
-    return const Text("data");
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
+
+    return Center(
+      child: Stack(
+        children: <Widget>[
+          AnimatedPositioned(
+            top: topPosition(height),
+            bottom: bottomPosition(height),
+            duration: const Duration(seconds: 2),
+            curve: Curves.fastOutSlowIn,
+            child: GestureDetector(
+              onTap: changePosition,
+              child: _customBottomAppBar(width),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 
   @override
   String? get titleScreen => widget.infor?.name;
+
+  bottomPosition(double height) =>
+      _position == Positions.top ? height - 150.0 : 0.0;
+
+  topPosition(double height) =>
+      _position == Positions.bottom ? height - 180.0 : 0.0;
+
+  changePosition() {
+    setState(() {
+      switch (_position) {
+        case Positions.bottom:
+          _position = Positions.top;
+          break;
+        case Positions.top:
+          _position = Positions.all;
+          break;
+        case Positions.all:
+          _position = Positions.bottom;
+          break;
+      }
+    });
+  }
+
+  Widget _customBottomAppBar(double width) => Container(
+        width: width,
+        color: Colors.blue,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.menu,
+                color: Colors.white,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                color: Colors.white,
+              ),
+            )
+          ],
+        ),
+      );
 }
